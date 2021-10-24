d24af59cd733bed0f0ea6a7001e45aaabb39732b
samgabrail <sameh.gabrail@gmail.com>
2021-10-23 20:08:34 -0400

    explore overpass

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 Map English names of village locations to their native names
 
+## Process
+This my thinking process
 
+1. Get the key from the translator app for a village
+2. Look up this key in GIS and get the max and min long and lat coordinates
+3. Pass these coordinates into OSM to find the tags for the names of the village and hopefully find the native name there
 
--- main.py
+++ main.py
@@ -3,7 +3,12 @@ try:
 except ImportError:  # Graceful fallback if IceCream isn't installed.
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
 
+from typing import List
 import requests
+import overpy
+import json
+from pydantic import BaseModel
+
 
 ISO_COUNTRY_CODES = [
     "AZE",
@@ -30,10 +35,9 @@ ISO_COUNTRY_CODES = [
     "VNM",
     "THA",
 ]
-ic(len(ISO_COUNTRY_CODES))
 
 
-def getRequiredLang(country: str) -> list:
+def getRequiredLang(country: str) -> List:
     url = f"https://translator-api-qa.taethni.com/api/languages/country/{country}"
     res = requests.get(url)
     # ic(res.json())
@@ -41,16 +45,64 @@ def getRequiredLang(country: str) -> list:
     return languages
 
 
-def getVillages(country, language):
+def getVillages(country: str, language: str) -> List:
     url = f"https://translator-api-qa.taethni.com/api/Keys/{country}/{language}"
     res = requests.get(url)
     res = res.json()
-    return len(res)
+    return res
 
 
-totalVillageCount = 0
-for country in ISO_COUNTRY_CODES:
-    villageCount = getVillages(country, "en")
-    ic(country, getRequiredLang(country), villageCount)
-    totalVillageCount += villageCount
-ic(totalVillageCount)
+def getBoundaries(country):
+    url = f"https://services.arcgis.com/jpbmZUwK24tWjkHJ/ArcGIS/rest/services/{country}_1_0_0/FeatureServer/4/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
+    res = requests.get(url)
+    return res.json()
+
+
+# Print all the languages needed per country along with the number of villages
+# totalVillageCount = 0
+# for country in ISO_COUNTRY_CODES:
+#     villageCount = getVillages(country, "en")
+#     ic(country, getRequiredLang(country), villageCount)
+#     totalVillageCount += villageCount
+# ic(totalVillageCount)
+
+
+# res = getBoundaries("IND")
+# ic(res)
+
+# api = overpy.Overpass()
+# # fetch all ways and nodes
+# result = api.query("""
+# [out:json][timeout:25];
+# area[name="Andorra"]->.searchArea;
+# (
+#   node[place~"city|town|village|hamlet"](area.searchArea);
+# );
+# out body;
+# >;
+# out skel qt;
+# """)
+
+# ic(len(result.nodes))
+# nodes = result.nodes
+# ic(nodes)
+
+villageCount = getVillages("EGY", "en")
+countryLang = getRequiredLang("EGY")
+name = villageCount[0]["enValue"]
+name = "Giza"
+ic(name)
+ic(countryLang)
+overpass_url = "http://overpass-api.de/api/interpreter"
+overpass_query = """
+[out:json];
+area["ISO3166-1"="EG"][admin_level=2];
+(node["name:en"="{0}"](area);
+);
+out center;
+"""
+overpass_query = overpass_query.format(name)
+ic(overpass_query)
+response = requests.get(overpass_url, params={"data": overpass_query})
+data = response.json()
+ic(data)
